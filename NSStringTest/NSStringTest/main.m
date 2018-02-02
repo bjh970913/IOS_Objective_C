b092e67acb90720289285b4579c6d09509750211
백지훈 <bjh@sdst.co.kr>
2018-02-02 16:55:22 +0900

    Init

---
+++ NSStringTest/NSStringTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSStringTest
+//
+//  Created by 백지훈 on 2018. 2. 2..
+//  Copyright © 2018 bjh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+
+}
